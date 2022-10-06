30c59439e9dea4c8c02949ba629ca0f7e640bb77
Thiago Santos <t.mateus.santos2004@gmail.com>
2022-10-06 15:51:45 +0100

    Update _app.js

--- metodologiasdev/pages/_app.js
+++ metodologiasdev/pages/_app.js
@@ -2,8 +2,14 @@ import '../styles/globals.css'
 import Navbar from '../components/NavBar'
 
 function MyApp({ Component, pageProps }) {
+  return (
+    <div>
   <Navbar/>
-  return <Component {...pageProps} />
+  
+  <Component {...pageProps} />
+  </div>
+  )
+
 }
 
 export default MyApp
