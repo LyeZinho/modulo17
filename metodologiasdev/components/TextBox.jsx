59ffaeb834ae13087489b0d21fcc2107d58fff75
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-10-12 10:00:03 +0100

    Logo

--- metodologiasdev/components/TextBox.jsx
+++ metodologiasdev/components/TextBox.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+
+//Component for render a text box
+
+function TextBox(props) {
+  return (
+    <div>
+      <label>{props.label}</label>
+      <input type="text" />
+    </div>
+  );
+}
