16446df1c376c1096fa8308d4701d915a1d35527
Thiago Santos <t.mateus.santos2004@gmail.com>
2022-10-13 15:13:39 +0100

    estrutura do texto

--- metodologiasdev/pages/index.jsx
+++ metodologiasdev/pages/index.jsx
@@ -1,12 +1,12 @@
 import Header from "../components/Header"
-
+import ImportanciaMetodologia from "../components/ImportanciaMetodologia"
 
 export default function Home() {
   return (
     <div>
 
       <Header />
-    
+      <ImportanciaMetodologia />
 
     </div>
 
