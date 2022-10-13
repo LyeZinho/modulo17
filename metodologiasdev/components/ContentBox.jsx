1a302f583de3b1333e0ee5f7815bc5908272060b
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-10-13 15:13:16 +0100

    Update ContentBox.jsx

--- metodologiasdev/components/ContentBox.jsx
+++ metodologiasdev/components/ContentBox.jsx
@@ -5,14 +5,42 @@ function Text(props) {
   return (
     <div>
       if (props.orientation === "left") {
-        <h1>
+        <div>
+          <div>
           <Image
             src={props.image == null || props.image == "" ? "" : props.image}
             alt={props.alt}
             width={props.width}
-            height={props.height}
+            height={props.height} 
           />
-        </h1> 
+        </div>
+        <div>
+          <h1>
+            {props.title == null || props.title == "" ? "" : props.title}
+          </h1>
+          <br />
+          <p>{props.text}</p>
+        </div> 
+      </div>
+      }
+      else {
+        <div>
+          <div>
+            <Image
+              src={props.image == null || props.image == "" ? "" : props.image}
+              alt={props.alt}
+              width={props.width}
+              height={props.height} 
+            />
+          </div>
+          <div>
+            <h1>
+              {props.title == null || props.title == "" ? "" : props.title}
+            </h1>
+            <br />
+            <p>{props.text}</p>
+          </div> 
+        </div>
       }
     </div>
   );
