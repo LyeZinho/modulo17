7a360a76fd69296c5a48a645ae50d0c2fd2f976e
Thiago Santos <t.mateus.santos2004@gmail.com>
2022-10-13 22:34:13 +0100

    setinha

--- metodologiasdev/pages/index.jsx
+++ metodologiasdev/pages/index.jsx
@@ -1,11 +1,15 @@
 import Header from "../components/Header"
 import ImportanciaMetodologia from "../components/ImportanciaMetodologia"
+import Setinha from "../components/Setinha"
+
 
 export default function Home() {
   return (
     <div>
       <Header />
       <ImportanciaMetodologia />
+      <Setinha />
+      
     </div>
 
   )
