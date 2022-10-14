fa8a05a2d15e2d4ae958d8121b606634630c7bcf
Thiago Santos <t.mateus.santos2004@gmail.com>
2022-10-14 22:42:04 +0100

    organização

--- metodologiasdev/components/Agile.jsx
+++ metodologiasdev/components/Agile.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
 
 const Agile = () => {
   return (
-    <div>Agile</div>
-  )
-}
+    <div className="min-h-full flex items-center justify-center py-16 px-4 sm:px-6 lg:px-8 ">
+      <div className="flex flex-col  items-center justify-center mt-6 space-x-10 pb-8 pt-8">
+        <div className="flex pt-6">
+          <div className="flex flex-col">
+            <h2>AGILE</h2>
+            <br />
+            <h7>
+              A metodologia AGILE é um ramo da engenharia de software que
+              enfatiza a entrega contínua de software em funcionamento.
+              <br />
+              Ele defende o planejamento adaptativo, desenvolvimento evolutivo,
+              entrega antecipada e melhoria contínua.
+              <br />
+              Essa abordagem ajuda as organizações a responder rapidamente às
+              necessidades em constante mudança de seus clientes.
+              <br />
+              A abordagem Agile foi proposta pela primeira vez por um grupo de
+              desenvolvedores de software em 2001.
+              <br />
+              <p>
+                {" "}
+                Eles criaram 12 princípios que acreditavam que ajudariam a criar
+                software melhor mais rapidamente. Esses princípios foram
+                posteriormente adaptados por outras organizações e ficaram
+                conhecidos como Manifesto Ágil.
+              </p>
+              <br />
+              O Manifesto Ágil consiste em quatro valores:
+              <br />
+              1) Indivíduos e interações mais que processos e ferramentas;
+              <br />  
+              2) Software funcionando mais que documentação abrangente;
+              <br />
+              3) Colaboração do cliente mais que negociação de contratos;
+              <br />
+              4) Responder a mudanças mais que seguir um plano;
+            </h7>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Agile
+export default Agile;
--- metodologiasdev/components/FerramentasExemplos.jsx
+++ metodologiasdev/components/FerramentasExemplos.jsx
@@ -4,44 +4,16 @@ import Image from "next/image";
 const FerramentasCaseExemplos = () => {
   return (
     <div className="min-h-full flex items-center justify-center py-16 px-4 sm:px-6 lg:px-8 ">
-      <div className="flex flex-col  items-center justify-center mt-6 space-x-10 pb-8 pt-8">
-        <div className="flex pt-6">
-          <Image
-            src="/image/assets/design-and-development-process.svg"
-            alt="/"
-            width="900"
-            height="900"
-          />
-
-          <div className="flex flex-col">
-            <h2>Ferramentas Case</h2>
-            <br />
-            <h7>
-              As ferramentas CASE são ferramentas para engenharia de software, que 
-              são concebidas para ajudar no desenvolvimento, manutenção e reengenharia de 
-              sistemas de software. As ferramentas CASE podem ser utilizadas para uma variedade 
-              de tarefas, tais como recolha de requisitos, planejamento, engenharia inversa, concepção e modelação, 
-              geração de códigos e testes. 
-            </h7>
-            <br />
-            <h7>
-              As principais vantagens das ferramentas CASE são:
-            </h7>
-            <ul>
-              <li>
-                ‣ Ajudam a automatizar o processo de desenvolvimento de software.
-              </li>
-              <li>
-                ‣ Melhoram a qualidade dos sistemas de software.
-              </li>
-              <li>
-                ‣ Ajudam a reduzir o custo de desenvolvimento de sistemas de software.
-              </li>
-            </ul>
-          </div>
+    <div className="flex flex-col  items-center justify-center mt-6 space-x-10 pb-8 pt-8">
+      <div className="flex pt-6">
+        <div className="flex flex-col">
+          <h2>Exemplos de Ferramentas CASE</h2>
+          <br />
         </div>
+
       </div>
     </div>
+  </div>
   );
 };
 
