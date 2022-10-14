e5ad9ecbb644af2baa923c39ea813c297f37fd97
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-10-14 22:08:33 +0100

    Changes

--- metodologiasdev/pages/index.jsx
+++ metodologiasdev/pages/index.jsx
@@ -2,16 +2,26 @@ import Header from "../components/Header"
 import ImportanciaMetodologia from "../components/ImportanciaMetodologia"
 import FerramentasCase from "../components/FerramentasCase"
 import Setinha from "../components/Setinha"
-
+import Scrum from "../components/Scrum"
+import EngenhariaDeSoftware from "../components/EngenhariaDeSoftware"
+import DiferentesPerfis from "../components/DiferentesPerfis"
+import CiclodevidaDev from "../components/CiclodevidaDev"
+import Agile from "../components/Agile"
+import CaracteristicasSoftware from "../components/CaracteristicasSoftware"
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <EngenhariaDeSoftware />
+      <DiferentesPerfis />
+      <CiclodevidaDev />
+      <CaracteristicasSoftware />
       <ImportanciaMetodologia />
+      <Scrum />
+      <Agile />
       <FerramentasCase />
       <Setinha />
-      
     </div>
 
   )
